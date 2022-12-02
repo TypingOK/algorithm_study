767c3f9718226ddcfbc20edb302e3d3cb4cceb91
Jin Jeong <wjdwls717@gmail.com>
2022-12-02 14:27:29 +0900

    2022-12-02

--- problem/N과 M (2).py
+++ problem/N과 M (2).py
@@ -9,19 +9,25 @@ visited = [False] * N
 select = []
 
 
-def comb(idx, depth):
+def comb(idx, start):
     if(idx == M):
         answer = " ".join(map(str, select))
         print(answer)
         return
-    if(depth == N):
-        return
+    # if(depth == N):
+    #     return
 
-    select.append(numbers[depth])
-    comb(idx+1, depth+1)
+    # select.append(numbers[depth])
+    # comb(idx+1, depth+1)
 
-    select.pop()
-    comb(idx, depth+1)
+    # select.pop()
+    # comb(idx, depth+1)
+    for i in range(start, N):
+        visited[i] = True
+        select.append(numbers[i])
+        comb(idx+1, i+1)
+        visited[i] = False
+        select.pop()
 
 
 comb(0, 0)
